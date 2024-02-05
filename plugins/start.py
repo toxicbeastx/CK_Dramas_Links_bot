50f3f642ec4415533d8885708c3ffa09afc97968
Toxic Beast <146731343+toxicbeastx@users.noreply.github.com>
2024-02-05 15:30:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,7 +55,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩...")
         try:
             messages = await get_messages(client, ids)
         except:
